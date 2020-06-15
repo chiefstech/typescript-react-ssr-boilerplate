7141a4d19ff3be4a6498ef42173ee231240d97be
Nelson Omuto <nelsonomuto@gmail.com>
2020-06-15 17:18:12 -0400

    clean up effect list navigation

--- src/common/util/storybook-utils/navigation.util.tsx
+++ src/common/util/storybook-utils/navigation.util.tsx
@@ -9,14 +9,6 @@ interface StoryContainerProps {
 
 export const StoryContainer: React.FC<StoryContainerProps> = ({ navigationState, children }) => {
   const dispatch = useDispatch();
-  dispatch({
-    type: 'tes'
-  });
-  type c = typeof navigationSlice.actions.updateCurrentRoute;
-  (navigationSlice.actions.updateCurrentRoute as c)('Home');
-  // useEffect(() => {
-  //   dispatch(navigationSlice.actions.updateCurrentRoute('Home'));
-  // }, []);
   dispatch(navigationSlice.actions.initialize(navigationState));
   return <React.Fragment>{children}</React.Fragment>;
 };
