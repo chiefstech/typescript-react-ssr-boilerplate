c9df457b37ceee9d48ba0a397f32e71b6920157e
Nelson Omuto <nelsonomuto@gmail.com>
2020-06-21 21:45:32 -0400

    update build to use nodemon

--- scripts/build.js
+++ scripts/build.js
@@ -3,16 +3,35 @@ const [client, server] = require('../config/webpack/webpack.config');
 
 const { NODE_ENV } = process.env;
 const compiler = webpack([client(NODE_ENV), server(NODE_ENV)]);
-compiler.watch({}, (err, stats) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
 
-  console.log(
-    stats.toString({
-      chunks: false, // Makes the build much quieter
-      colors: true // Shows colors in the console
-    })
-  );
-});
+if (NODE_ENV === 'development') {
+  console.log('👷‍♂️ Running in development mode 🙌');
+  compiler.watch({}, (err, stats) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(
+      stats.toString({
+        chunks: false, // Makes the build much quieter
+        colors: true // Shows colors in the console
+      })
+    );
+  });
+} else {
+  console.log('🎬 Cutting a build: production mode 🙌');
+  compiler.run((err, stats) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(
+      stats.toString({
+        chunks: false, // Makes the build much quieter
+        colors: true // Shows colors in the console
+      })
+    );
+  });
+}
